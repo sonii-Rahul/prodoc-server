d84e8db3262606546cb876878c46b7654c813df3
Rahul Soni <rahuls5788@gmail.com>
2024-05-14 02:00:45 +0530

    first commit

--- src/app.js
+++ src/app.js
@@ -2,6 +2,14 @@ import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import session from "express-session"
+import path from "path";
+import { fileURLToPath } from "url";
+
+//resolving dir name 
+
+const __filename=fileURLToPath(import.meta.url)
+const __dirname=path.dirname(__filename)
+console.log(__dirname);
 
 const app = express();
 
@@ -40,3 +48,12 @@ app.use("/api/v1/users", userRouter);
 export default app;
 
 //exporting app
+
+//use the cliend app
+app.use(express.static(path.join(__dirname,'/client/dist')))
+
+//render client 
+
+app.get('*',(req,res)=>{
+    res.sendFile(path.join(__dirname,'/client/dist/index.html'))
+})
