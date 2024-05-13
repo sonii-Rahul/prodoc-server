cf1f7316cccd938f8bcc24b753cc5b4404a6a7b6
Rahul Soni <rahuls5788@gmail.com>
2024-05-14 00:18:56 +0530

    added secure =true

--- src/app.js
+++ src/app.js
@@ -27,7 +27,8 @@ app.use(session({
     saveUninitialized: false,
     cookie: { 
         maxAge: 1000 * 60 * 60 * 24 ,
-        sameSite: 'lax'
+        sameSite: 'lax',
+        secure: true
     }
 }))
 
